7e902e48dd1e1972bb0ddb755ab0642d2021af2f
VladPron <vladislavpron0@gmail.com>
2023-09-08 14:09:31 +0300

    back

--- frontend/src/interfaces/search.interface.ts
+++
@@ -1,9 +0,0 @@
-import { Order } from "../enums/order-enum";
-
-export interface SearchConditionI {
-  search: string;
-  order: Order | null;
-  minPrice: number | null;
-  maxPrice: number | null;
-  rate: number | null;
-}
--- frontend/src/services/axios.service.ts
+++
@@ -1,9 +0,0 @@
-import axios, { AxiosInstance } from "axios";
-
-const baseURL = "http://localhost:5000";
-
-interface AxiosService extends AxiosInstance {}
-
-const axiosService: AxiosService = axios.create({ baseURL });
-
-export { axiosService };
